150b572d3121d576e56a9b44dbdf7cd00a016a66
Brian Knoles <bknoles@gmail.com>
2025-11-19 09:44:03 -0800

    Add chatroom features

--- app/channels/chat_channel.rb
+++ app/channels/chat_channel.rb
@@ -1,2 +1,26 @@
 class ChatChannel < ApplicationCable::Channel
+  def subscribed
+    reject unless current_user
+    stream_from "chat"
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+
+  def speak(data)
+    return unless current_user
+
+    message = ChatMessage.create!(
+      body: data["body"],
+      user: current_user
+    )
+
+    ActionCable.server.broadcast("chat", {
+      id: message.id,
+      body: message.body,
+      user: { username: message.user.username },
+      created_at: message.created_at
+    })
+  end
 end
